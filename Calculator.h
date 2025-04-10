bcbad49afd37f4e405a4e07959d8eef3c816a33d
Arsenika83 <arsenika83@gmail.com>
2025-04-10 21:52:21 +0300

    Changes: - Добавлен файл Calculator.h - Изменён файл Main.cpp

---
+++ Calculator.h
@@ -0,0 +1,12 @@
+//заголовочный файл калькулятора
+
+class Calculator {
+    public:
+        void menu();
+        double add(double x, double y);
+        double subtract(double x, double y);
+        double multiply(double x, double y);
+        double divide(double x, double y);
+        double power(double x, int power);
+    private:
+};
